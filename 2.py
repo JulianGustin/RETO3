530ab6b33b4b3422a4bd265b7157a0ad739e799b
JulianGustin <158980531+JulianGustin@users.noreply.github.com>
2024-03-12 22:47:26 -0500

    Delete 2.py

--- 2.py
+++
@@ -1,9 +0,0 @@
-x = float (input("escribe un numero: "))
-y = float (input("escribe otro numero:"))
-z = float (input("es ribe el ultimo numero: "))
-if x > y > z: 
-    print(str(x) + " es el mayor numero")
-elif z > y > x:
-    print(str(z) + " es el mayor numero")
-else:
-    print(str(y) + " es el mayor numero")
